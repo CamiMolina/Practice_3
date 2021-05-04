0e853911187cb981da00172213da22db0a8c6a5e
CamiMoli <molinatcami@gmail.com>
2021-05-04 11:49:02 -0400

    P3-001: Se hicieron modificaciones en Student

--- Trabajo_3/Trabajo_3/Student.cs
+++ Trabajo_3/Trabajo_3/Student.cs
@@ -2,14 +2,11 @@ using System;
 
 namespace Trabajo_3
 {
-    public class WeatherForecast
+    public class Student
     {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public class Student
+        {
+            public String NameStudent { get; set; }
+        }
     }
 }
